7d593c5d9d453851cd1291fc82b2cef030f01da0
Stone <XSTStone@gmail.com>
2023-04-27 17:19:25 +0800

    注册唯一性功能

---
+++ static/infos.py
@@ -0,0 +1,5 @@
+reg_err_infos = [' ',
+                 'Username has been used. Please choose another one.',
+                 "Two passwords don't match. Please check again.",
+                 'Username, password and confirming password should be all filled',
+                 'Unknown error type']
