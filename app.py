a66ee633693cce5713f0cb04c4394eec7ca16fef
XSTStone <57900292+XSTStone@users.noreply.github.com>
2023-05-01 15:27:43 +0800

    完成登录用户信息记录

--- app.py
+++ app.py
@@ -3,14 +3,30 @@ from flask import render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import traceback
+from flask_wtf import FlaskForm
 from static.config import conn, DB_URI
 from static import infos
+from flask_login import LoginManager, login_user, login_required, current_user, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from user.user import User_Session
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    password_cfm = StringField('Password Confirm', validators=[DataRequired()])
+
 
 app = Flask(__name__)
 app.secret_key = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'account_login'
 
 
 @app.route('/home1')
@@ -31,6 +47,13 @@ def default_page():
 @app.route('/home')
 def home_logged():
     return render_template('index.html')
+
+
+@app.route('/logged')
+@login_required
+def index():
+    print('GOT In')
+    return render_template('index_web.html', username=current_user.username)
 
 
 # 模型评估
@@ -98,8 +121,10 @@ def account_register_check():
     elif password != password2:
         return redirect(url_for('account_register_err_handler', err_type=infos.reg_err_types[1]))
     else:
-        new_user = Users(username=username, password=password, id=None)
-        users = Users.query.filter(Users.username == username).first()
+        password_hash = generate_password_hash(password)
+        print(password_hash)
+        new_user = Users_db(username=username, password=password_hash, id=None)
+        users = Users_db.query.filter(Users_db.username == username).first()
         if users is not None:
             print(users.username)
             return redirect(url_for('account_register_err_handler', err_type=infos.reg_err_types[0]))
@@ -119,7 +144,8 @@ def account_register_err_handler(err_type):
     elif err_type == infos.reg_err_types[2]:
         return render_template('account_register.html', err_info=infos.reg_err_infos[2], err_type=err_type)
     else:
-        return render_template('account_register.html', err_info=infos.reg_err_infos[3], err_type=infos.reg_err_types[3])
+        return render_template('account_register.html', err_info=infos.reg_err_infos[3],
+                               err_type=infos.reg_err_types[3])
 
 
 @app.route('/account-login', methods=['POST', 'GET'])
@@ -128,27 +154,120 @@ def account_login():
 
 
 @app.route('/login/check', methods=['POST'])
-def login_check():
+def account_login_check():
     print('Got in check')
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
 
-        user = Users.query.filter(Users.username == username, Users.password == password).first()
+        user = Users_db.query.filter(Users_db.username == username, Users_db.password == password).first()
         if user:
             return redirect(url_for('home'))
         else:
             return redirect(url_for('account_login'))
 
 
-@app.route('/account-')
+@app.route('/account-my')
+def account_my():
+    return None
 
 
-class Users(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User_Session.get(user_id=user_id)
+
+
+def get_user_in_db(username):
+    user = Users_db.query.filter(Users_db.username == username).first()
+    if user:
+        return user
+    else:
+        return None
+
+
+class Users_db(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     username = db.Column(db.String(10), unique=True)
-    password = db.Column(db.String(16))
+    password = db.Column(db.String(200))
+
+    def get_username(self):
+        return self.username
+
+    def get_id(self):
+        return self.id
+
+    def get_password(self):
+        return self.password
+
+
+class User_Session(UserMixin):
+    """登陆_User类"""
+
+    def __init__(self, user):
+        self.username = user.get_username()
+        self.password_hash = user.get_password()
+        self.id = user.get_id()
+
+    def verify_password(self, password):
+        """密码验证"""
+        if self.password_hash is None:
+            return False
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        """获取用户ID"""
+        return self.id
+
+    @staticmethod
+    def get(user_id):
+        """根据用户ID获取User Entity，为login_user提供支持"""
+        if not user_id:
+            return None
+        found_user = Users_db.query.filter(Users_db.id == user_id).first()
+        if found_user:
+            return found_user
+        else:
+            return None
+
+
+@app.route('/account-login-web', methods=('GET', 'POST'))
+def account_login_web():
+    print('Got in login web')
+    form = LoginForm()
+    err_msg = None
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        password_cfm = form.password_cfm.data
+        print('username = ', username)
+        print('password = ', password)
+        found_user_in_db = get_user_in_db(username=username)
+        if found_user_in_db is None:
+            err_msg = 'User not exist'
+        elif password != password_cfm:
+            err_msg = 'Different password'
+        else:
+            user = User_Session(found_user_in_db)
+            if user.verify_password(password):
+                login_user(user)
+                return redirect(request.args.get('next') or url_for('index'))
+            else:
+                err_msg = 'Wrong password'
+    else:
+        print('Not in')
+    return render_template('login.html', form=form, err_msg=err_msg)
+
+
+@app.route('/account-login-web-page', methods=('GET', 'POST'))
+def account_login_web_page():
+    form = LoginForm()
+    return render_template('login.html', form=form, err_msg='empty')
+
+
+@app.route('/account-register-web-page', methods=('GET', 'POST'))
+def account_register_web_page():
+    return render_template('account_register.html')
 
 
 if __name__ == '__main__':
--- user/user.py
+++ user/user.py
@@ -1,15 +1,43 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash
 
 Base = declarative_base()
 
+
 class User(Base):
+    """数据库_User类"""
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(50))
     password = Column(String(50))
-    role = Column(Integer) # 0 for admin, 1 for user
 
-def create_user(username, password, role=1):
-    user = User(username, password, )
 
+class User_Session(UserMixin):
+    """登陆_User类"""
+    def __init__(self, user):
+        self.username = user.get_username()
+        self.password_hash = user.get_password()
+        self.id = user.get_id()
+
+    def verify_password(self, password):
+        """密码验证"""
+        if self.password_hash is None:
+            return False
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        """获取用户ID"""
+        return self.id
+
+    @staticmethod
+    def get(user_id):
+        """根据用户ID获取User Entity，为login_user提供支持"""
+        if not user_id:
+            return None
+        found_user = Users_db.query.filter(Users_db.id == user_id).first()
+        if found_user:
+            return found_user
+        else:
+            return None
